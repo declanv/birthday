fc27c46712689ff33e1c0a3206a53fbbcca330d7
Declan Van Welie <declanv@gmail.com>
2019-12-19 20:01:32 -0500

    Adding: initial attempt at determining whether a birthday is upcoming or has passed. Trickier than I initially thought, since we tend to think of 'upcoming' as being in the upcoming 6 months, not the past since months

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import './App.scss';
 const randomUserUrl = 'https://randomuser.me/api/?results=20&nat=us,ca&inc=id,name,gender,nat,dob,picture&noinfo';
 
+
 class BirthdayList extends React.Component {
   constructor(props) {
     super(props);
@@ -14,9 +15,31 @@ class BirthdayList extends React.Component {
   sortUsers() {
     console.log('sorting users');
   }
+  checkBirthday(userDob) {
+    let today = new Date();
+    let todayMonth = today.getMonth();
+    let todayDay = today.getDate();
+    let dob = new Date(userDob);
+    let birthdayMonth = dob.getMonth();
+    let birthdayDay = dob.getDate();
+
+    if (todayMonth === birthdayMonth && todayDay === birthdayDay) {
+      console.log(`Birthday Today!: Today's month: ${todayMonth}, and BirthdayMonth: ${birthdayMonth}. Today's day, and birthdayDay: ${birthdayDay}`);
+      return "Birthday Today!";
+    //  This is actually pretty tricky
+    } else if ((todayMonth - birthdayMonth <= 6 && todayMonth > birthdayMonth) || (todayMonth === birthdayMonth && todayDay > birthdayDay)) {
+      console.log(`Birthday Upcoming": Today's month: ${todayMonth}, and BirthdayMonth: ${birthdayMonth}. Today's day, and birthdayDay: ${birthdayDay}`)
+      return "Birthday Upcoming";
+    } else {
+      console.log(`Birthday Passed": Today's month: ${todayMonth}, and BirthdayMonth: ${birthdayMonth}. Today's day, and birthdayDay: ${birthdayDay}`)
+      return "Birthday Passed";
+    }
+
+  }
   componentDidMount() {
     axios.get(randomUserUrl)
         .then(response => {
+
           this.setState({users: response.data.results});
           console.log(response.data.results);
         })
@@ -27,6 +50,7 @@ class BirthdayList extends React.Component {
   render() {
     let userList = this.state.users.length > 0 ? this.state.users.map((step, i) => {
         let user = this.state.users[i];
+        let birthdayStatus = this.checkBirthday(user.dob.date);
         return (
             <div key={user.dob.date}>
               <p>
