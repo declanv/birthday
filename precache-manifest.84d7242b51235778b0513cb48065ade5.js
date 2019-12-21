5aced74cc681e488ca360a93d5365b1b7f0169c1
Declan Van Welie <declanv@gmail.com>
2019-12-21 14:35:23 -0500

    Updates

---
+++ precache-manifest.84d7242b51235778b0513cb48065ade5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8824beee04cc5a7070b5aeca71fcc086",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "d714a1ef464b4cb67b83",
+    "url": "/birthday/static/css/main.4b1a0ec2.chunk.css"
+  },
+  {
+    "revision": "7545741a9548f1b105e9",
+    "url": "/birthday/static/js/2.8044e911.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/birthday/static/js/2.8044e911.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d714a1ef464b4cb67b83",
+    "url": "/birthday/static/js/main.2f261abe.chunk.js"
+  },
+  {
+    "revision": "2d5e1157498b7347cc02",
+    "url": "/birthday/static/js/runtime-main.6f23c34f.js"
+  },
+  {
+    "revision": "7c470b8ae5119f430ce08f6b94288b99",
+    "url": "/birthday/static/media/TofinoProPersonal-Regular.7c470b8a.otf"
+  },
+  {
+    "revision": "f05d262ec2281fdfb1ea9fe822e76692",
+    "url": "/birthday/static/media/TofinoProPersonalCond-Light.f05d262e.otf"
+  },
+  {
+    "revision": "be005ffc249558903135457935514e80",
+    "url": "/birthday/static/media/TofinoProPersonalCond-Medium.be005ffc.otf"
+  },
+  {
+    "revision": "21ea2db5f766589c18fabf5ae202f1d6",
+    "url": "/birthday/static/media/TofinoProPersonalCond-Semibold.21ea2db5.otf"
+  },
+  {
+    "revision": "bdae5ddb9c631bf643616231aafc3559",
+    "url": "/birthday/static/media/TofinoProPersonalNarrow-Book.bdae5ddb.otf"
+  },
+  {
+    "revision": "7ec79a6fb8e1354ee3ac0c33ca57c90e",
+    "url": "/birthday/static/media/TofinoProPersonalWide-Thin.7ec79a6f.otf"
+  },
+  {
+    "revision": "59d438e68e46e362132ab88e24cabcd2",
+    "url": "/birthday/static/media/TofinoProPersonalWide-Ultra.59d438e6.otf"
+  }
+]);
